aa6ecb844f115418bf9f87378b5adc3e74960b4c
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2021-12-29 16:11:31 +0900

    モデルクラスにリレーションを設定する

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
-use App\Models\Task;
 
 class TaskController extends Controller
 {
@@ -14,7 +13,7 @@ class TaskController extends Controller
 
         $current_folder = Folder::find($id);
 
-        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
