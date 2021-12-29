00fe8ac940bf27d711d7a97e807c55d011df85e0
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2021-12-29 21:48:22 +0900

    タスクを編集する

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 use App\Models\Folder;
 use App\Models\Task;
 
@@ -52,6 +53,20 @@ class TaskController extends Controller
 
         return view('tasks/edit', [
             'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        $task = Task::find($task_id);
+
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
         ]);
     }
 }
