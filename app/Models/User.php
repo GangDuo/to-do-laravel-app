26c37da4cfcc0683915f845292bfe886f04cd972
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2021-12-30 18:44:52 +0900

    公式ドキュメントと書き方を揃える

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
 
     public function folders()
     {
-        return $this->hasMany('App\Models\Folder');
+        return $this->hasMany(Folder::class);
     }
 
     /**
