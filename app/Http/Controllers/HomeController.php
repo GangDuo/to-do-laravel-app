d8a4cfc6925a0ddef25ccacebbadae735a2dfd73
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2021-12-30 19:43:02 +0900

    Route Model Binding

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
             return view('home');
         }
         return redirect()->route('tasks.index', [
-            'id' => $folder->id,
+            'folder' => $folder->id,
         ]);
     }
 }
