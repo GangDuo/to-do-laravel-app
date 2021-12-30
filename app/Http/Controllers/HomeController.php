15d624e6551b401885777807088093ddd7434b7c
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2021-12-30 14:47:10 +0900

    フォルダを作成済みであればタスク一覧ページにリダイレクト

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $folder = Auth::user()->folders()->first();
+        if (is_null($folder)) {
+            return view('home');
+        }
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
 }
