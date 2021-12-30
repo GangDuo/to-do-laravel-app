291ae0ef31a527e1ef3d88dda710dedf63e80ff9
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2021-12-30 20:36:20 +0900

    他人のフォルダへアクセスを禁止する

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,15 @@ use App\Http\Controllers\FolderController;
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
-    Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-    Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
-    Route::post('/folders/{folder}/tasks/create', [TaskController::class, 'create']);
+    Route::group(['middleware' => 'can:view,folder'], function() {
+        Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
-    Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-    Route::post('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit']);
+        Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', [TaskController::class, 'create']);
+    
+        Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit']);
+    });
 
     Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
     Route::post('/folders/create', [FolderController::class, 'create']);
